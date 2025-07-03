01bf6b0ba3144341bcb4f5a7caad601d989d5f2f
sanchaybaghel <sanchaybaghel92@gmail.com>
2025-07-03 11:42:23 +0530

    payment added

--- server/index.js
+++ server/index.js
@@ -39,6 +39,7 @@ app.use('/api/shopkeeper', require('./routes/Admin'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/payment', require('./routes/payment'));
 app.use('/api/orders', require('./routes/order'));
+app.use('/api/admin', require('./routes/Admin'));
 
 
 
